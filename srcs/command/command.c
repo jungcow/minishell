c308d263edc0c03b88b4acb64cced277ba4eecae
seunghoh <seunghoh@student.42seoul.kr>
2021-04-14 22:30:46 +0900

    [refactor] : Modify terminal Module to Command
    기존에 terminal 모듈을 통해 터미널 입, 출력 관리를 하려고 했는데
    이게 minishell.c 와 의미상 겹치는 부분도 있고
    terminal/command/ 식으로 command 모듈을 terminal 모듈 아래에 두는게
    부자연스럽게 느껴져서 terminal 모듈 이름을 command 모듈로 변경했음

--- srcs/command/command.c
+++ srcs/command/command.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   command.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/14 22:18:05 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/04/14 22:28:59 by seunghoh         ###   ########.fr       */
+/*   Created: 2021/04/14 21:55:18 by seunghoh          #+#    #+#             */
+/*   Updated: 2021/04/14 22:27:53 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "command/command.h"
 
-int		main()
+int		read_command()
 {
-	read_command();
-	printf("hello world\n");
-	return (0);
+	return (1);
 }
