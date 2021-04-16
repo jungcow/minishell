706a2480b05658ce5cee9a84103b7e8dd7999617
jungcow <jungwkim@student.42seoul.kr>
2021-04-17 06:59:31 +0900

    [feat] : Add capability structure and position structure
    termcap이랑 cursor position을 좀더 간편하게 이용하기 위한 구조체를 선언함.

--- includes/command/terminal.h
+++ includes/command/terminal.h
@@ -6,7 +6,7 @@
 /*   By: jungwkim <jungwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 06:05:34 by jungwkim          #+#    #+#             */
-/*   Updated: 2021/04/17 06:09:25 by jungwkim         ###   ########.fr       */
+/*   Updated: 2021/04/17 06:40:00 by jungwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ typedef struct		s_cap
 	char			*cm; //move curser
 	char			*im; //turn on insert mode
 	char			*ei; //turn off insert mode (actually, it is switch)
-	char			*ce; // delete all characters before cursors
+	char			*cd; // delete all lines below the cursor
+	char			*ce; // delete all characters before cursor
 	char			*dc; // delete one characters at cursors
 	t_pos			pos;
 }					t_cap;
