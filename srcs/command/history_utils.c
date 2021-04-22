f3252b31beb4e0a69ead3cd92a4caf7c6b17c715
Jungwoo Kim <jungwkim@c6r9s6.42seoul.kr>
2021-04-22 20:23:52 +0900

    [fix] : move cursor back

--- srcs/command/history_utils.c
+++ srcs/command/history_utils.c
@@ -6,7 +6,7 @@
 /*   By: jungwkim <jungwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 21:03:59 by jungwkim          #+#    #+#             */
-/*   Updated: 2021/04/22 16:12:55 by jungwkim         ###   ########.fr       */
+/*   Updated: 2021/04/22 20:20:46 by jungwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void		write_historyfd(t_history *new, int fd)
 	if (fd > 2)
 		write(fd, "\n", 1);
 }
-
+/*
 static void	get_command_lines(t_command *command, t_term *term,
 					t_history *prev_history, int prev_flag)
 {
@@ -33,13 +33,10 @@ static void	get_command_lines(t_command *command, t_term *term,
 		command_line_len = command->present->line.length;
 	else
 		command_line_len = prev_history->line.length;
-	term->pos.row = (command_line_len + name_len - 1) / term->pos.col;
+	term->pos.row = (command_line_len + name_len) / term->pos.col;
 	term->pos.row -= name_len / term->pos.col;
-	if (name_len % term->pos.col == 0)
-		if (command_line_len == 0)
-			if (term->pos.cur_col == 0)
-				term->pos.row += 1;
 }
+*/
 
 static void	save_prev(t_history *history, t_history **prev_history,
 												int flag, int *prev_flag)
@@ -55,16 +52,35 @@ static void	save_prev(t_history *history, t_history **prev_history,
 		*prev_history = history;
 	}
 }
-// name_len이 col이랑 같을 때 문제가 생김 <- 이부분 해결 요망
+
+void		move_cursor_back(t_command *command, t_term *term,
+							t_history *prev_history, int prev_flag)
+{
+//	while (history->cursor)
+	int		len;
+	int		key;
+
+	(void)term;
+	key = LEFT_ARROW;
+	if (!prev_flag)
+		len = command->present->line.length;
+	else
+		len = prev_history->line.length;
+	while (len--)
+		write(1, &key, sizeof(key));
+}
+
 void		write_historyline(t_command *command, t_term *term,
 							t_history *history, int flag)
 {
 	static int			prev_flag;
 	static t_history	*prev_history;
 
-	get_command_lines(command, term, prev_history, prev_flag);
-	term->pos.cur_row -= term->pos.row;
-	tputs(tgoto(term->cp.cm, ft_strlen(term->name) % term->pos.col, term->pos.cur_row), 1, tputs_wrapper);
+	move_cursor_back(command, term, prev_history, prev_flag);
+//	get_command_lines(command, term, prev_history, prev_flag);
+//	term->pos.cur_row -= term->pos.row;
+//	tputs(tgoto(term->cp.cm, ft_strlen(term->name) % term->pos.col,
+//							term->pos.cur_row), 1, tputs_wrapper);
 	tputs(term->cp.cd, 1, tputs_wrapper);
 	if (flag)
 	{
