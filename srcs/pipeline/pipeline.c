57e6c3266d0920160b6bb4c214fdc1716b459868
jungcow <jungwkim@student.42seoul.kr>
2021-04-28 14:14:45 +0900

    [temp] : Save temp for switch local
    42서울 아이맥으로 작업하기 위해 임시저장함

--- srcs/pipeline/pipeline.c
+++ srcs/pipeline/pipeline.c
@@ -37,6 +37,7 @@ int		check_parse_necessary(char *filename)
 {
 	struct stat	dump;
 	char		*command;
+	char		*
 	int			i;
 
 	command = filename;
@@ -46,34 +47,54 @@ int		check_parse_necessary(char *filename)
 		if (command[i] == '/')
 		{
 			command[i] = '\0';
-			if (stat(command, &dump) != 1)
-				return (0);
+			if (stat(command, &dump) != -1)
+				if (has_file(command, filename))
+					return (1);
+				else
+					break ;
+			else
+				break ;
 		}
 	}
-	return (1);
+	return (0);
 }
 
 char	*parse_path(char *filename)
 {
 	char	**path;
+	char	*tmp;
 	int		i;
 
 	path = ft_split(getenv("PATH"), ':');
 	if (path == NULL)
 		return (NULL);
-	if (!check_parse_necessary(filename))
-		return (NULL);
+	if (check_parse_necessary(filename))
+		return (ft_strdup(""));
 	i = -1;
 	while (path[++i])
 		if (has_file(path[i], filename))
-			return (path[i]);
-	return (NULL);
+		{
+			tmp = ft_strdup(path[i]);
+			if (tmp == NULL)
+				return (NULL);
+			break;
+		}
+	i = -1;
+	while (path[++i])
+		free(path[i]);
+	free(path);
+	return (tmp);
 }
 
 int		run_child_process(t_pipeline *pipeline, int *fd, int idx)
 {
-	if (idx != 0)
+	char	*dir;
+	char	*input;
+	char	*ouput;
+
+	if (idx == 0)
 	{
+		close(fd[0]);
 	}
 	dir = parse_path(pipeline->operation->name);
 	dir = ft_strjoin(dir, pipeline->operation->name);
@@ -81,16 +102,15 @@ int		run_child_process(t_pipeline *pipeline, int *fd, int idx)
 		return (0);
 	dup2()
 }
-		
-
 
 int		run_xx(t_pipeline *pipeline)
 {
-	int		idx;
 	int		fd[2];
 	pid_t	*pid;
-	char	*dir;
+	int		idx;
 
+	if (!pipeline || !pipeline->operation || !pipeline->length)
+		return (0);
 	if (pipe(fd) < 0)
 		return (0);
 	pid = (t_pid *)malloc(sizeof(t_pid) * pipeline->length);
