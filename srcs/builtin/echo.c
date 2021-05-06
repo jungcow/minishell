a9bcd1c4dc66b672ae992e3dfafaee7f28eab47d
오승환 <audiotrack8619@gmail.com>
2021-05-06 22:07:08 +0900

    [fix] : fix echo -nnnnnn possible
    echo -nnnnn 되도록 변경함

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,13 +6,32 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 19:29:31 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/04 18:31:23 by seunghoh         ###   ########.fr       */
+/*   Updated: 2021/05/06 22:06:22 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <errno.h>
 #include "libft.h"
+
+int		check_option(char *arg)
+{
+	int		i;
+	int		len;
+
+	i = 1;
+	len = ft_strlen(arg);
+	if (len > 1)
+	{
+		if (arg[0] != '-')
+			return (0);
+		while (i < len)
+			if (arg[i++] != 'n')
+				return (0);
+		return (1);
+	}
+	return (0);
+}
 
 int		ft_echo(int argc, char **argv)
 {
@@ -21,7 +40,7 @@ int		ft_echo(int argc, char **argv)
 
 	i = 1;
 	new_line = true;
-	if (argc > 1 && ft_strcmp(argv[1], "-n") == 0)
+	if (argc > 1 && check_option(argv[1]))
 	{
 		new_line = false;
 		i++;
