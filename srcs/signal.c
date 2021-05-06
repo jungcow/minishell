775f61fe4842d90d50606c675af1e6ce953aa558
jungcow <jungwkim@student.42seoul.kr>
2021-05-06 20:31:28 +0900

    [chore] : Fix SIGINT handler problems while child process is running
    자식 프로세스가 돌아가고 있을 때 ctrl+ C 를 누르면 한칸 개행이 되어야 하는 현상 해결

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 16:39:04 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/06 20:23:01 by seunghoh         ###   ########.fr       */
+/*   Updated: 2021/05/06 20:29:55 by jungwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ void	sigint_handler(void)
 		write(1, TERM_NAME, ft_strlen(TERM_NAME)); 
 		g_command.exit_status = 1;
 	}
+	else
+		write(1, "\n", 1);
 }
 
 void	sigquit_handler(void)
