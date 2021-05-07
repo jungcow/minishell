2422e98007dfbe770eebfe3ff5001cc02e919798
Seunghwan Oh <seunghoh@c10r1s6.42seoul.kr>
2021-05-07 22:25:18 +0900

    [chore] : ft_cd prototype spell check

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 18:35:17 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/07 22:24:32 by seunghoh         ###   ########.fr       */
+/*   Updated: 2021/05/07 22:25:01 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int		ft_echo(int argc, char **argv, t_environ *environ);
 int		ft_pwd(void);
-nt		ft_cd(int argc, char **argv);
+int		ft_cd(int argc, char **argv);
 int		ft_export(int argc, char **argv, t_environ *environ);
 int		ft_unset(int argc, char **argv, t_environ *environ);
 int		ft_env(int argc, char **argv, t_environ *environ);
