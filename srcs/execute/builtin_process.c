f817035d1c3689f6b069084fc413a6f31da38221
Jungwoo Kim <jungwkim@c10r1s4.42seoul.kr>
2021-05-07 22:28:19 +0900

    [chore] : Fix ft_cd arguments
    ft_cd의 인자에서 env를 받지 않는 것으로 고침

--- srcs/execute/builtin_process.c
+++ srcs/execute/builtin_process.c
@@ -6,7 +6,7 @@
 /*   By: jungwkim <jungwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 04:23:28 by jungwkim          #+#    #+#             */
-/*   Updated: 2021/05/07 22:17:24 by jungwkim         ###   ########.fr       */
+/*   Updated: 2021/05/07 22:26:53 by jungwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int		ft_execve(char *command, char **argv, t_environ *environ)
 	if (ft_strcmp(command, "echo") == 0)
 		ret = ft_echo(ft_strslen(argv), argv, environ);
 	else if (ft_strcmp(command, "cd") == 0)
-		ret = ft_cd(ft_strslen(argv), argv, environ);
+		ret = ft_cd(ft_strslen(argv), argv);
 	else if (ft_strcmp(command, "pwd") == 0)
 		ret = ft_pwd();
 	else if (ft_strcmp(command, "env") == 0)
