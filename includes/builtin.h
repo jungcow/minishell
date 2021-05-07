9455194a4b7d05ae93f37e63624f039df9bb8b87
Seunghwan Oh <seunghoh@c10r1s6.42seoul.kr>
2021-05-07 22:24:39 +0900

    [fix] : Modify ft_cd prototype
    ft_cd env 사용 안하도록 수정함

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 18:35:17 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/07 22:18:22 by jungwkim         ###   ########.fr       */
+/*   Updated: 2021/05/07 22:24:32 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int		ft_echo(int argc, char **argv, t_environ *environ);
 int		ft_pwd(void);
-int		ft_cd(int argc, char **argv, t_environ *environ);
+nt		ft_cd(int argc, char **argv);
 int		ft_export(int argc, char **argv, t_environ *environ);
 int		ft_unset(int argc, char **argv, t_environ *environ);
 int		ft_env(int argc, char **argv, t_environ *environ);
