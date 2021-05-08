c162181e0f0933fef41e5ca01176690044404df8
Seunghwan Oh <seunghoh@c6r5s2.42seoul.kr>
2021-05-08 20:08:41 +0900

    [fix] : signal fix
    pid 가 0인지 아닌지에 따라서 다르게 설정함
    [pid == 0]
    ctrl + c => status 1
    ctrl + \ => do nothing
    
    [pid != 0]
    ctrl + c => status 130 + write \n
    ctrl + \ => status 131 + write ~~

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 16:39:04 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/08 01:03:42 by jungwkim         ###   ########.fr       */
+/*   Updated: 2021/05/08 20:08:08 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,13 @@ void	sigint_handler(void)
 			exit(1);
 		write(1, "\n", 1);
 		write(1, TERM_NAME, ft_strlen(TERM_NAME));
+		g_command.exit_status = 1;
 	}
-	g_command.exit_status = 130;
+	if (g_command.pid != 0)
+	{
+		write(1, "\n", 1);
+		g_command.exit_status = 130;
+	}
 }
 
 void	sigquit_handler(void)
@@ -42,6 +47,6 @@ void	sigquit_handler(void)
 	if (g_command.pid != 0)
 	{
 		write(1, "Quit: 3\n", ft_strlen("Quit: 3\n"));
+		g_command.exit_status = 131;
 	}
-	g_command.exit_status = 131;
 }
