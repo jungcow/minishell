04585aa4405bc6a5938a437731ff9b61df2941b4
jungcow <jungwkim@student.42seoul.kr>
2021-05-15 13:34:30 +0900

    [fix] : fixed norm finally

--- srcs/parse/parse_util.c
+++ srcs/parse/parse_util.c
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 19:51:29 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/15 13:22:30 by seunghoh         ###   ########.fr       */
+/*   Updated: 2021/05/15 13:31:43 by jungwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int		skip_dquote(char *str, int length, int start)
 {
 	int		i;
 	int		j;
+
 	i = 0;
 	start++;
 	while (start < length)
